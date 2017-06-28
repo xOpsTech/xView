0583e5bea97ba2ff63e3dc2f0ab0375754cc64b5
Gayantha Samaradiwakara <gns@gnslk.com>
2017-06-28 19:58:28 +0530

    Business Component

--- src/app/business/business.component.ts
+++ src/app/business/business.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertService } from '../services/alert.service';
+import { CircleProgressComponent } from '../../shared/circle-progress.component';
 
 @Component({
   selector: 'app-business',
   templateUrl: './business.component.html',
-  styleUrls: ['./business.component.scss']
+  styleUrls: ['./business.component.scss'],
+  providers: [AlertService]
 })
 export class BusinessComponent implements OnInit {
 
-  constructor() { }
+  public alert_trend;
+
+  constructor(private alertsService: AlertService) {
+
+    this.alertsService.getAlertTrends('12')
+    .subscribe((data: any) => {
+      this.alert_trend = data;
+      //console.log(this.alert_trend);
+    });
+
+  }
 
   ngOnInit() {
   }
