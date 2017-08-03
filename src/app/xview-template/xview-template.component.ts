4cb7daa6ddf10d7bf0299613bf6f8ed5e165f714
Mahendra Tennakoon <mahendrathennakoon@gmail.com>
2017-08-03 18:49:43 +0530

    change logo based on tenant

--- src/app/xview-template/xview-template.component.ts
+++ src/app/xview-template/xview-template.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService, } from '../services/user.service';
 @Component({
   selector: 'app-xview-template',
   templateUrl: './xview-template.component.html',
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class XviewTemplateComponent implements OnInit {
 
-  constructor() { }
+  imgURL:string
+  user = {
+    name: "",
+    picture: ""
+  };
+  constructor(private userService:UserService) { }
 
   ngOnInit() {
+    this.userService.getUserData().subscribe(res => {
+      this.user = res;
+      this.userService.setUserName(this.user.name);
+      this.updateURLs();
+    });
+  }
+
+  updateURLs() {
+    console.log(this.userService.getUsername());
+    if (this.userService.getUsername().trim() == 'John Edwards') {
+      this.imgURL = "/assets/img/logo/scholastic.png";
+    } else {
+      this.imgURL = "/assets/img/logo.png";
+    }
   }
 
 }
