9379455b9d5922c0f1d657926f3a2df9c766790e
Samaraweera <VSAMAKE@PEROOT.COM>
2017-10-25 21:48:33 +0530

    logsearch tenant add

--- src/app/logsearch/logsearch.component.ts
+++ src/app/logsearch/logsearch.component.ts
@@ -36,25 +36,21 @@ private search:boolean = true;
     console.log(searchLogsForm.value)
     
     console.log(this.searchq);
+    console.log(this.tenant_id);
     // hide iframe
     if(this.search == true){  
       this.searchq = encodeURI(searchLogsForm.value.searchq);
-      this.logsearchurl = this.sanitizer.bypassSecurityTrustResourceUrl("http://35.196.242.246:5601/app/kibana#/dashboard/dc3ccc90-add9-11e7-b735-1d7e32f6ffa5?embed=true&_g=()&_a=(description:%27%27,filters:!(),options:(darkTheme:!f),panels:!((col:1,columns:!(_source),id:%273c9776f0-add8-11e7-8cb5-cd6bd3193b0e%27,panelIndex:1,row:1,size_x:12,size_y:7,sort:!(timestamp,desc),type:search)),query:(query_string:(analyze_wildcard:!t,query:%27_index:%22metrics-fvowgm8%22%20AND%20" + this.searchq + "%27)),timeRestore:!f,title:%27Search%20Dashboard%27,uiState:(),viewMode:view)");
+      this.logsearchurl = this.sanitizer.bypassSecurityTrustResourceUrl("http://35.196.242.246:5601/app/kibana#/dashboard/dc3ccc90-add9-11e7-b735-1d7e32f6ffa5?embed=true&_g=()&_a=(description:%27%27,filters:!(),options:(darkTheme:!f),panels:!((col:1,columns:!(_source),id:%273c9776f0-add8-11e7-8cb5-cd6bd3193b0e%27,panelIndex:1,row:1,size_x:12,size_y:7,sort:!(timestamp,desc),type:search)),query:(query_string:(analyze_wildcard:!t,query:%27_index:%22metrics-"+ this.tenant_id+"%22%20AND%20" + this.searchq + "%27)),timeRestore:!f,title:%27Search%20Dashboard%27,uiState:(),viewMode:view)");
       this.search = false; 
   }
     else{
       this.searchq = encodeURI(searchLogsForm.value.searchq);
-      this.logsearchurl = this.sanitizer.bypassSecurityTrustResourceUrl("http://35.196.242.246:5601/app/kibana#/dashboard/dc3ccc90-add9-11e7-b735-1d7e32f6ffa5?embed=true&_g=()&_a=(description:%27%27,filters:!(),options:(darkTheme:!f),panels:!((col:1,columns:!(_source),id:%273c9776f0-add8-11e7-8cb5-cd6bd3193b0e%27,panelIndex:1,row:1,size_x:12,size_y:7,sort:!(timestamp,desc),type:search)),query:(query_string:(analyze_wildcard:!t,query:%27_index:%22metrics-fvowgm8%22%20AND%20" + this.searchq + "%27)),timeRestore:!f,title:%27Search%20Dashboard%27,uiState:(),viewMode:view)");
+      this.logsearchurl = this.sanitizer.bypassSecurityTrustResourceUrl("http://35.196.242.246:5601/app/kibana#/dashboard/dc3ccc90-add9-11e7-b735-1d7e32f6ffa5?embed=true&_g=()&_a=(description:%27%27,filters:!(),options:(darkTheme:!f),panels:!((col:1,columns:!(_source),id:%273c9776f0-add8-11e7-8cb5-cd6bd3193b0e%27,panelIndex:1,row:1,size_x:12,size_y:7,sort:!(timestamp,desc),type:search)),query:(query_string:(analyze_wildcard:!t,query:%27_index:%22metrics-"+ this.tenant_id+"%22%20AND%20" + this.searchq + "%27)),timeRestore:!f,title:%27Search%20Dashboard%27,uiState:(),viewMode:view)");
       this.search = true;
     }
-    console.log(this.logsearchurl);
-   // show iframe
-
-
-
+  
   }
   ngOnInit() {
-
     this.userService.getUserData().subscribe(res => {
       console.log(res.id);
       this.user = res;
@@ -62,6 +58,7 @@ private search:boolean = true;
       this.tenant_id = this.user.tenantId;
       console.log(this.tenant_id);
     });
+    this.logsearchurl = this.sanitizer.bypassSecurityTrustResourceUrl("http://35.196.242.246:5601/app/kibana#/dashboard/dc3ccc90-add9-11e7-b735-1d7e32f6ffa5?embed=true&_g=()&_a=(description:%27%27,filters:!(),options:(darkTheme:!f),panels:!((col:1,columns:!(_source),id:%273c9776f0-add8-11e7-8cb5-cd6bd3193b0e%27,panelIndex:1,row:1,size_x:12,size_y:7,sort:!(timestamp,desc),type:search)),query:(query_string:(analyze_wildcard:!t,query:%27_index:%22metrics-"+ this.tenant_id+"%22%20AND%20*%27)),timeRestore:!f,title:%27Search%20Dashboard%27,uiState:(),viewMode:view)");
 
   }
 
