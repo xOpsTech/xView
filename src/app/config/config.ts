5521b5d65246f50fb5dc2ce2416069a058e7e660
Samaraweera <VSAMAKE@PEROOT.COM>
2017-10-27 03:13:11 +0530

    config change

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -2,7 +2,7 @@ let dev = 'http://localhost:4200';
 let stg = 'http://localhost:4200';
 let prod = 'http://xview.xops.it';
 
-let host = dev;
+let host = prod;
 
 export const config = {
     XOPSHOST: host,
